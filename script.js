641e9bd5fb17fd82c28caa1124f615ec55f8e528
chloe posthuma-coelho <96831510+chloeposthumacoelho@users.noreply.github.com>
2022-08-05 14:25:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,37 @@
-// INNOVATION
-var slideIndex = 1;
-showSlides(slideIndex);
+console.log('we are in')
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+let theme = localStorage.getItem('theme')
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+let themeDots = document.getElementsByClassName('theme-dot')
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'blue.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'green.css'
+	}
+
+	if(mode == 'purple'){
+		document.getElementById('theme-style').href = 'purple.css'
+	}
+	localStorage.setItem('theme', mode)
 }
-
-
-// for Google maps api
-
-"use strict";
-
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: -34.397,
-      lng: 150.644
-    },
-    zoom: 8
-  });
-}
-
